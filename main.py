1f004f3d5cf3767af8e75f201bbeface5087748e
JOGA <j.gaj@gmail.com>
2017-11-26 15:03:56 +0100

    if-else

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-imie = 'Asia'
-print ("Hello world! lalalal, {0}!".format(imie))
+#imie = 'Asia'
+#nazwisko = "G"
+#print ("Hello world! lalalal, {0}!".format(imie) +" "+ nazwisko)
+
+#zwierzeta = 4
+#cena = 40 / 4
+#print (cena)
+
+def wypisz_osobe(imie, nazwisko):
+    print ("{0} {1}".format(imie, nazwisko))
+
+def  czy_emeryt(wiek):
+    MAX = 68
+    if wiek < MAX:
+        return True
+    else:
+        return  False
+
+if __name__ == "__main__":
+    imie = 'asia'
+    nazwisko = 'G'
+    wiek = 85
+    print (imie)
+
+    wypisz_osobe(imie, nazwisko)
+    if czy_emeryt(wiek):
+        print('nie emeryt')
+    else:
+        print ('emeryt')
